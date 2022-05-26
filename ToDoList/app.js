2698a97ce2c92202f0c3b6c0d7d3fd31fe4042fb
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2022-05-26 17:44:29 +0900

    Base functionaloity complete

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -5,6 +5,9 @@ const todoList = document.querySelector('.todo-list');
 //event listeners
 
 todoButton.addEventListener('click', addTodo)
+
+todoList.addEventListener('click',deleteCheck);
+
 //functions
 
 function addTodo(event){
@@ -38,4 +41,19 @@ function addTodo(event){
     todoList.appendChild(todoDiv);
     //clear todoInput.value
     todoInput.value= "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //DELETE
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    //check mark
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
